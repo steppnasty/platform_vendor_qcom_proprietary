5aee31a03c400a144d9830a920bf9168bc4774d1
steppnasty <steppnasty@gmail.com>
2016-01-31 17:01:16 -0500

    Included jpeg encoding props

--- qcom-vendor.mk
+++ qcom-vendor.mk
@@ -56,12 +56,16 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/proprietary/system/lib/libDivxDrm.so:/system/lib/libDivxDrm.so \
     vendor/qcom/proprietary/system/lib/libdnshostprio.so:/system/lib/lilbdnshostprio.so \
     vendor/qcom/proprietary/system/lib/libExtendedExtractor.so:/system/lib/libExtendedExtractor.so \
+    vendor/qcom/proprietary/system/lib/libfastcvopt.so:/system/lib/libfastcvopt.so \
     vendor/qcom/proprietary/system/lib/libgsl.so:/system/lib/libgsl.so \
+    vendor/qcom/proprietary/system/lib/libjpegehw.so:/system/lib/libjpegehw.so \
     vendor/qcom/proprietary/system/lib/libmm-color-convertor.so:/system/lib/libmm-color-convertor.so \
     vendor/qcom/proprietary/system/lib/libmmosal.so:/system/lib/libmmosal.so \
+    vendor/qcom/proprietary/system/lib/libmmqjpeg_codec.so:/system/lib/libmmqjpeg_codec.so \
     vendor/qcom/proprietary/system/lib/libnetmonitor.so:/system/lib/libnetmonitor.so \
     vendor/qcom/proprietary/system/lib/libOpenVG.so:/system/lib/libOpenVG.so \
     vendor/qcom/proprietary/system/lib/libqc-opt.so:/system/lib/libqc-opt.so \
+    vendor/qcom/proprietary/system/lib/libqomx_jpegenc.so:/system/lib/libqomx_jpegenc.so \
     vendor/qcom/proprietary/system/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
     vendor/qcom/proprietary/system/lib/libtcpfinaggr.so:/system/lib/libtcpfinaggr.so \
     vendor/qcom/proprietary/system/lib/libv8.so:/system/lib/libv8.so \
